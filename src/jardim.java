b1943248aacf3ef25e909ceee1389cf39ef77239
ead <ead@academico.local>
2025-05-09 10:30:57 -0300

    logica-aula7-ex2

--- src/jardim.java
+++ src/jardim.java
@@ -4,8 +4,18 @@ import javax.swing.JOptionPane;
 public class jardim {
     public static void main(String[] args) throws Exception {
         JOptionPane.showMessageDialog(null, "Alô, Jardim!");
-        JOptionPane.showMessageDialog(null,"Digite seu nome:");
-        JOptionPane.showMessageDialog(null, "Digite seu endereço:");
+        
+        String nomeCliente = JOptionPane.showInputDialog(null, "Digite seu nome:");
+            if(nomeCliente == null){
+                JOptionPane.showMessageDialog(null, "Operação cancelada pelo usuário");
+                return;
+            }
+        String enderecoCliente = JOptionPane.showInputDialog(null, "Digite o seu endereço:");
+            if(enderecoCliente == null){
+                JOptionPane.showMessageDialog(null, "Operação cancelada!");
+                return;
+            }
+        JOptionPane.showMessageDialog(null,"Dados do cliente:\nNome:" + nomeCliente + "\nEndereço:" + enderecoCliente);
 
     }
     
