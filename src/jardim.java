ff9a177f5760c96a7ce2a16e3297557dc3852b91
ead <ead@academico.local>
2025-05-09 09:00:52 -0300

    ok

---
+++ src/jardim.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JOptionPane;
+
+public class jardim {
+    public static void main(String[] args) throws Exception {
+        JOptionPane.showMessageDialog(null, "Alô, Jardim!");
+        JOptionPane.showMessageDialog(null,"Digite seu nome:");
+        JOptionPane.showMessageDialog(null, "Digite seu endereço:");
+
+    }
+    
+}
+
